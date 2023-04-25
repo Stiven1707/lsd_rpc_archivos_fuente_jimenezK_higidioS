075f8a21353b6b132f353a9f68d8a2e57be4d34f
stibenDavid <shigidio@unicauca.edu.co>
2023-04-25 18:10:11 -0500

    se agrega case 1, por verificar

--- cliente/cliente.c
+++ cliente/cliente.c
@@ -81,7 +81,56 @@ gestion_usuarios_1(char *host)
 		switch (opcion)
 		{
 		case 1:
-			//TODO
+			// Limpiar búfer de entrada
+			fgets(registrarusuario_1_arg.nombres, MAXNOM, stdin);
+			// Ingresar datos del cliente
+			printf("\nIngrese nombres: ");
+			fgets(registrarusuario_1_arg.nombres, MAXNOM, stdin);
+			registrarusuario_1_arg.nombres[strcspn(registrarusuario_1_arg.nombres, "\n")] = '\0';
+			fflush(stdin);
+
+			printf("\nIngrese apellidos: ");
+			fgets(registrarusuario_1_arg.apellidos, MAXNOM, stdin);
+			registrarusuario_1_arg.apellidos[strcspn(registrarusuario_1_arg.apellidos, "\n")] = '\0';
+			fflush(stdin);
+			
+			printf("Ingrese correo electrónico: ");
+			fgets(registrarusuario_1_arg.correo, MAXCORREO, stdin);
+			registrarusuario_1_arg.correo[strcspn(registrarusuario_1_arg.correo, "\n")] = '\0';
+			fflush(stdin);
+			
+			printf("Ingrese teléfono: ");
+			fgets(registrarusuario_1_arg.telefono, MAXTEL, stdin);
+			registrarusuario_1_arg.telefono[strcspn(registrarusuario_1_arg.telefono, "\n")] = '\0';
+			fflush(stdin);
+			
+			printf("Ingrese nombre de usuario: ");
+			fgets(registrarusuario_1_arg.login, MAXNOM, stdin);
+			registrarusuario_1_arg.login[strcspn(registrarusuario_1_arg.login, "\n")] = '\0';
+			fflush(stdin);
+			
+			printf("Ingrese contraseña: ");
+			fgets(registrarusuario_1_arg.contrasenia, MAXNOM, stdin);
+			registrarusuario_1_arg.contrasenia[strcspn(registrarusuario_1_arg.contrasenia, "\n")] = '\0';
+			fflush(stdin);
+			
+			// Asignar tipo de usuario
+			registrarusuario_1_arg.tipo = CLIENT;
+			// Imprimir los datos del administrador ingresado
+			printf("\nDatos del cliente ingresado:\n");
+			printf("Nombres: %s\n", registrarusuario_1_arg.nombres);
+			printf("Apellidos: %s\n", registrarusuario_1_arg.apellidos);
+			printf("Correo electrónico: %s\n", registrarusuario_1_arg.correo);
+			printf("Teléfono: %s\n", registrarusuario_1_arg.telefono);
+			printf("Nombre de usuario: %s\n", registrarusuario_1_arg.login);
+			printf("Contraseña: %s\n", registrarusuario_1_arg.contrasenia);
+			printf("Tipo de usuario: %d\n", registrarusuario_1_arg.tipo);
+			result_1 = registrarusuario_1(&registrarusuario_1_arg, clnt1);
+			if (result_1 == (bool_t *) NULL) {
+				clnt_perror(clnt1, "call failed");
+			} else if (*result_1 == TRUE) {
+				printf("\n Usuario registrado exitosamente.");
+			}
 			break;
 		case 2:
 			while (result_2->codigo != 1) {
